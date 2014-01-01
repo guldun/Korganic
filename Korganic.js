8ef494edcc340734d7bc4bb3e85f3b31c704f239
Joshua Lawson <>
2014-01-01 18:10:13 +1100

    Add hexToRGB to Korganic and remove from Circle, so that it can be used elsewhere

--- Korganic.js
+++ Korganic.js
@@ -117,5 +117,14 @@ Korganic.prototype = {
     
     setContext: function(context){
         this.context = context;
+    },
+    
+    hexToRgb: function(hex){
+        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+        return result ? {
+            r: parseInt(result[1], 16),
+            g: parseInt(result[2], 16),
+            b: parseInt(result[3], 16)
+        }: null;
     }
 };
