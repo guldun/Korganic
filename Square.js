6033a7e934c4962d9b0339f0ead74d8c29416ded
Joshua Lawson <>
2014-01-01 17:45:39 +1100

    Add Square JS file

---
+++ Square.js
@@ -0,0 +1,23 @@
+/* 
+ * Korganic
+ * 
+ * By Joshua Lawson - SphyrixNetworks
+ * 
+ * ----------------
+ * 
+ * HTML5 Animation software built for use with KGE (Koron Game Engine) for building
+ * 2D Animations and Menus for games built with KGE. For information about KGE 
+ * please visit http://www.kge.com.au for information about Korganic visit 
+ * http://kge.com.au/tools/korganic or for any information about the author of 
+ * either of the mentioned software please visit http://joshualawson.com.au or
+ * http://sphyrix.net
+ * 
+ * ----------------
+ * 
+ * e-mail: josh@sphyrix.net
+ * twitter: jlawson1988
+ * 
+ * ----------------
+ * This software is licensed under the MIT License
+ */
+
