bb0591962574336bc19d4d96874a3f2b65e855ab
Joshua Lawson <>
2014-01-01 19:51:53 +1100

    Changed the way Sqaure draws

--- Square.js
+++ Square.js
@@ -36,34 +36,11 @@ var Square = function(context, settings)
         stroke: settings.stroke || false,
         stroke_width: settings.stroke_width || 1,
         stroke_opacity: settings.stroke_opacity || 1,
-        opacity: settings.opacity || 100
+        opacity: settings.opacity || 100,
+        rotation: settings.rotaion || 0
     };
     
-    this.context.beginPath();
-    this.context.fillRect(this.getX(),this.getY(),this.getWidth(), this.getHeight());
-    
-    //Check if there is an overall opacity, if there is adjust the individual opacitys to include the overall opacity
-    if(this.getOpacity() !== 100)
-    {
-        this.setStrokeOpacity(this.getStrokeOpacity() * (1 - (parseFloat(this.getOpacity()) / 100.0)));
-        this.setFillOpacity(this.getFillOpacity() * (1 - (parseFloat(this.getOpacity()) / 100.0)));
-    }
-    
-    //If fill is set or stroke is set
-    if(this.getFill() !== false)
-    {    
-        this.context.fillStyle = "rgba(" + hexToRgb(this.getFill()).r + ", " + hexToRgb(this.getFill()).g + ", " + hexToRgb(this.getFill()).b + ", " + this.getFillOpacity() + ")";
-        this.context.fill();
-    }
-    if(this.getStroke !== false)
-    {
-        this.context.strokeStyle = "rgba(" + hexToRgb(this.getStroke()).r + ", " + hexToRgb(this.getStroke()).g + ", " + hexToRgb(this.getStroke()).b + ", " + this.getStrokeOpacity() + ")";
-        this.context.lineWidth = this.getStrokeWidth();
-        this.context.stroke();
-    }
-        
-    this.context.closePath();
-    
+    this.draw();
 };
 
 Square.prototype = {
@@ -147,6 +124,42 @@ Square.prototype = {
     
     setStrokeWidth: function(stroke_width){
         this.settings.stroke_width = stroke_width;
+    },
+            
+    getRotation: function(){
+        return this.settings.rotation;
+    },
+    
+    setRotation: function(rotation){
+        this.settings.rotation = rotation;
+    },
+            
+    draw: function()
+    {
+        this.context.beginPath();
+        this.context.fillRect(this.getX(),this.getY(),this.getWidth(), this.getHeight());
+
+        //Check if there is an overall opacity, if there is adjust the individual opacitys to include the overall opacity
+        if(this.getOpacity() !== 100)
+        {
+            this.setStrokeOpacity(this.getStrokeOpacity() * (1 - (parseFloat(this.getOpacity()) / 100.0)));
+            this.setFillOpacity(this.getFillOpacity() * (1 - (parseFloat(this.getOpacity()) / 100.0)));
+        }
+
+        //If fill is set or stroke is set
+        if(this.getFill() !== false)
+        {    
+            this.context.fillStyle = "rgba(" + hexToRgb(this.getFill()).r + ", " + hexToRgb(this.getFill()).g + ", " + hexToRgb(this.getFill()).b + ", " + this.getFillOpacity() + ")";
+            this.context.fill();
+        }
+        if(this.getStroke !== false)
+        {
+            this.context.strokeStyle = "rgba(" + hexToRgb(this.getStroke()).r + ", " + hexToRgb(this.getStroke()).g + ", " + hexToRgb(this.getStroke()).b + ", " + this.getStrokeOpacity() + ")";
+            this.context.lineWidth = this.getStrokeWidth();
+            this.context.stroke();
+        }
+
+        this.context.closePath();
     }
 };
 
